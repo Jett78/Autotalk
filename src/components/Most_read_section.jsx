597cbb3e742fdcd651a649eb0feae4ea1d5563d4
AyushSths <ayushsthx088@gmail.com>
2024-05-01 23:05:10 +0545

    edit

--- src/components/Most_read_section.jsx
+++ src/components/Most_read_section.jsx
@@ -5,6 +5,8 @@ import image_52 from '../assets/image 52.png'
 import image_59 from '../assets/image 59.png'
 import image_54 from '../assets/image 54.png'
 import user from '../assets/user.png'
+import { FaCircleUser } from "react-icons/fa6";
+
 
 function Most_read_section() {
     return (
@@ -49,15 +51,15 @@ function Most_read_section() {
                             </div>
                         </div>
                         <div className="lower-section mt-5 md:mt-10">
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 ">
-                                <div className="sm:border border-slate-200 rounded-xl">
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
+                                <div className="mt-10 sm:border border-slate-200 rounded-xl sm:mt-0 md:w-[427px] h-[329px]">
                                     <div className="md:shrink-0">
                                         <img className="h-full w-full object-contain md:h-full  md:w-full" src={image_52} alt="" />
                                     </div>
-                                    <div className="mt-3 sm:p-2 md:p-5">
+                                    <div className="px-6 py-4">
                                         <div className=" text-slate-600 font-medium tracking-wider mb-3 md:text-lg">2023 Porsche Panamera launched in India, priced from Rs 1.68 crore</div>
-                                        <div className="flex ... text-slate-700 leading-snug text-xs gap-2 md:text-sm md:gap-3">
-                                            <p className='flex'><img src={user} alt="" className='relative bottom-1 md:bottom-0.5 mr-1' /> Admin</p>
+                                        <div className="text-slate-700 sm:text-[14px] text-[12px] flex items-center justify-start gap-4 ">
+                                            <p className='flex items-center gap-1'><img src={user} alt="" /> Admin</p>
                                             <p>.</p>
                                             <p>4-wheeler</p>
                                             <p>.</p>
@@ -65,14 +67,15 @@ function Most_read_section() {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="mt-10 sm:border border-slate-200 rounded-xl sm:mt-0">
+
+                                <div className="mt-10 sm:border border-slate-200 rounded-xl sm:mt-24 md:w-[427px] h-[329px] md:mt-0">
                                     <div className="md:shrink-0">
                                         <img className="h-full w-full object-contain md:h-full  md:w-full" src={image_59} alt="" />
                                     </div>
-                                    <div className="mt-3 sm:p-2 md:p-5">
-                                        <div className="text-slate-600 font-medium tracking-wider mb-3 md:text-lg">Ferrari SF90 XX Stradale makes debut at Casa Ferrari Abu Dhabi</div>
-                                        <div className="flex ... text-slate-700 leading-snug text-xs gap-2 md:text-sm md:gap-3">
-                                            <p className='flex'><img src={user} alt="" className='relative bottom-1 md:bottom-0.5 mr-1' /> Admin</p>
+                                    <div className="px-6 py-4">
+                                        <div className=" text-slate-600 font-medium tracking-wider mb-3 md:text-lg">Ferrari SF90 XX Stradale makes debut at Casa Ferrari Abu Dhabi</div>
+                                        <div className="text-slate-700 sm:text-[14px] text-[12px] flex items-center justify-start gap-4 ">
+                                            <p className='flex items-center gap-1'><img src={user} alt="" /> Admin</p>
                                             <p>.</p>
                                             <p>4-wheeler</p>
                                             <p>.</p>
@@ -80,14 +83,14 @@ function Most_read_section() {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="mt-10 sm:border border-slate-200 rounded-xl sm:mt-0">
+                                <div className="mt-10 sm:border border-slate-200 rounded-xl sm:mt-24 md:w-[427px] h-[329px] md:mt-0">
                                     <div className="md:shrink-0">
                                         <img className="h-full w-full object-contain md:h-full  md:w-full" src={image_54} alt="" />
                                     </div>
-                                    <div className="mt-3 sm:p-2 md:p-5">
+                                    <div className="px-6 py-4">
                                         <div className=" text-slate-600 font-medium tracking-wider mb-3 md:text-lg">New Kia Sonnet facelift unveil on Dec. 14, here are more details</div>
-                                        <div className="flex text-slate-700 leading-snug text-xs gap-2 md:text-sm md:gap-3">
-                                            <p className='flex'><img src={user} alt="" className='relative bottom-1 md:bottom-0.5 mr-1' /> Admin</p>
+                                        <div className="text-slate-700 sm:text-[14px] text-[12px] flex items-center justify-start gap-4 ">
+                                            <p className='flex items-center gap-1'><img src={user} alt="" /> Admin</p>
                                             <p>.</p>
                                             <p>4-wheeler</p>
                                             <p>.</p>
@@ -99,7 +102,7 @@ function Most_read_section() {
                         </div>
                     </section>
                 </section >
-            </div >
+            </div>
         </>
     )
 }
