21d05922c774f4094eb477a35005ba680fc32cd0
Jeet <jetdeula@gmail.com>
2024-05-03 14:15:26 +0545

    all data added here

---
+++ src/db/Data.jsx
@@ -0,0 +1,89 @@
+export const innovation = [
+    {
+        img:"./cars/image 65.png",
+        title:"New Kia Sonnet facelift unveil on Dec. 14, here are more details",
+        name:"Admin",
+        type:"4-wheeler",
+        date:"Apr 23,2023",
+    },
+    {
+        img:"./cars/image 64.png",
+        title:"2023 Porsche Panamera launched in India, priced from Rs 1.68 crore",
+        name:"Admin",
+        type:"4-wheeler",
+        date:"Apr 23,2023",
+    },
+    {
+        img:"./cars/image 63.png",
+        title:"Scorpio drives demand for Mahindra SUVs with highest monthly bookings in Q2",
+        name:"Admin",
+        type:"4-wheeler",
+        date:"Apr 23,2023",
+    },
+    {
+        img:"./cars/image 59.png",
+        title:"Ferrari SF90 XX Stradale makes debut at Casa Ferrari Abu Dhabi",
+        name:"Admin",
+        type:"4-wheeler",
+        date:"Apr 23,2023",
+    },
+    {
+        img:"./cars/image 54.png",
+        title:"New Kia Sonnet facelift unveil on Dec. 14, here are more details",
+        name:"Admin",
+        type:"4-wheeler",
+        date:"Apr 23,2023",
+    },
+    {
+        img:"./cars/image 52.png",
+        title:"2023 Porsche Panamera launched in India, priced from Rs 1.68 crore",
+        name:"Admin",
+        type:"4-wheeler",
+        date:"Apr 23,2023",
+    },
+]
+
+
+export const cars = [
+    {
+        img:"./cars/image 59.png",
+        title:"Ferrari SF90 XX Stradale makes debut at Casa Ferrari Abu Dhabi",
+        type:"4-wheeler",
+        date:"Apr 23,2023",
+    },
+    {
+        img:"./cars/image 52.png",
+        title:"New Kia Sonnet facelift unveil on Dec. 14, here are more details",
+        type:"4-wheeler",
+        date:"Apr 23,2023",
+    },
+]
+export const bikes = [
+    {
+        img:"./bikes/bike1.png",
+        title:"2023 Royal Enfield Himalayan launched, prices start at Rs 2.69 lakh",
+        type:"2-wheeler",
+        date:"Apr 23,2023",
+    },
+    {
+        img:"./bikes/bike2.png",
+        title:"Orxa Mantis electric motorcycle launched, priced at Rs 3.6 lakh",
+        type:"2-wheeler",
+        date:"Apr 23,2023",
+    },
+]
+  export const scooters = [
+    {
+        img:"./scooters/scooter1.png",
+        title:"'Massive discount on Ola S1 X+, here are complete details",
+        type:"2-wheeler",
+        date:"Apr 23,2023",
+    },
+    {
+        img:"./scooters/scooter2.png",
+        title:"Bajaj Chetak electric scooter: Price, features & specifications",
+        type:"2-wheeler",
+        date:"Apr 23,2023",
+    },
+]
+  
