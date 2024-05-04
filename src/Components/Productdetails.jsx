81f1d9c48f204394d091cecb342587049f427fa7
Jeet <jetdeula@gmail.com>
2024-05-04 10:21:43 +0545

    table added

--- src/Components/Productdetails.jsx
+++ src/Components/Productdetails.jsx
@@ -1,28 +1,84 @@
 import React from 'react'
 
-const Productdetails = () => {
+const Productdetails = ({tesla}) => {
+    const{brand,model,releasedate,engine,milage,Transmission,brakes,seat,weigth,battery,range} = tesla;
   return (
-    <main>
+    <main className='lg:px-0 px-4 '>
         <h2 className="text-[24px] text-purple font-semibold border-b py-2">Details</h2>
-        <div>
+         {/* first row tables */}
+         <div className='flex flex-wrap justify-center gap-4 text-gray'>
             <section>
-                <h3>Product Information</h3>
-                <table className='border w-[650px]'>
-                    <tr className='border'>
+                <h3 className='py-4'>Product Information</h3>
+                <table className='tableborder  sm:w-[500px] w-[300px] lg:w-[650px] '>
+                    <tr className='tableborder'>
                         <td className='p-4'>Brand</td>
-                        <td>Image</td>
+                        <td>{brand}</td>
                     </tr>
-                    <tr className='border'>
-                        <td className='p-4'>Brand</td>
-                        <td>Image</td>
+                    <tr className='tableborder'>
+                        <td className='p-4'>Model</td>
+                        <td>{model}</td>
                     </tr>
-                    <tr className='border'>
-                        <td className='p-4'>Brand</td>
-                        <td>Image</td>
+                    <tr className='tableborder '>
+                        <td className='p-4'>Release Date</td>
+                        <td>{releasedate}</td>
                     </tr>
                 </table>
             </section>
-            <section></section>
+            <section>
+                <h3 className='py-4'>Power and Performance</h3>
+                <table className='tableborder  sm:w-[500px] w-[300px] lg:w-[650px]'>
+                    <tr className='tableborder'>
+                        <td className='p-4'>Engine Capacity</td>
+                        <td>{engine} bhp</td>
+                    </tr>
+                    <tr className='tableborder'>
+                        <td className='p-4'>Milage</td>
+                        <td>{milage} km/hr</td>
+                    </tr>
+                    <tr className='tableborder'>
+                        <td className='p-4'>Transmission</td>
+                        <td>{Transmission}</td>
+                    </tr>
+                </table>
+            </section>
+        </div>
+
+        {/* second row tables */}
+        <div className='flex flex-wrap justify-center gap-4 mt-6 text-gray'>  
+            <section>
+                <h3 className='py-4'>Key Highlights</h3>
+                <table className='tableborder  sm:w-[500px] w-[300px] lg:w-[650px]'>
+                    <tr className='tableborder '>
+                        <td className='p-4'>Brakes</td>
+                        <td>{brakes}</td>
+                    </tr>
+                    <tr className='tableborder '>
+                        <td className='p-4'>Battery</td>
+                        <td>{battery}</td>
+                    </tr>
+                    <tr className='tableborder'>
+                        <td className='p-4'>Range</td>
+                        <td>{range}</td>
+                    </tr>
+                </table>
+            </section>
+            <section>
+                <h3 className='py-4'>Key Highlights</h3>
+                <table className='tableborder  sm:w-[500px] w-[300px] lg:w-[650px]'>
+                    <tr className='tableborder'>
+                        <td className='p-4'>Seat Capacity</td>
+                        <td>{seat}</td>
+                    </tr>
+                    <tr className='tableborder'>
+                        <td className='p-4'>Kerb Weigth</td>
+                        <td>{weigth} Kg</td>
+                    </tr>
+                    <tr className='tableborder'>
+                        <td className='p-4'>Battery</td>
+                        <td>{battery}</td>
+                    </tr>
+                </table>
+            </section>
         </div>
     </main>
   )
