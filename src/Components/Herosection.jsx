2141b33d633a663f8765cd2e7632a08d5c5a27ff
Jeet <jetdeula@gmail.com>
2024-05-07 19:40:02 +0545

    animation added

--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -5,17 +5,17 @@ const Herosection = () => {
   return (
     <main className="bg-primary max-h-[750px] w-full mt-16 ">
        <div className="md:py-16 py-10">
-       <h2 className="font-semibold sm:text-[42px] px-4 text-xl  text-purple text-center mx-auto max-w-[1096px] tracking-wide leading-[145%]">
+       <h2 className="animate-slideleft font-semibold sm:text-[42px] px-4 text-xl  text-purple text-center mx-auto max-w-[1096px] tracking-wide leading-[145%]">
           Get Latest updates on what's going on AutoEcosystem in Nepal
         </h2>
-        <p className="py-4 px-1 sm:text-[16px] text-[14px] text-gray text-center mx-auto max-w-[1096px]  tracking-wide font-normal">
+        <p className="animate-slideright py-4 px-1 sm:text-[16px] text-[14px] text-gray text-center mx-auto max-w-[1096px]  tracking-wide font-normal">
           We cover trending and latest news on Cars & Bikes and publish it on{" "}
           <span className="text-purple font-semibold">Technews.today</span> on a
           daily basis. All resources published here are fact-checked and proof
           read so you can enjoy and get updates on what happening here at Nepal.
         </p>
         <figure>
-          <img src="./div.png" alt="herosection" className="mx-auto" />
+          <img src="./div.png" alt="herosection" className="mx-auto animate-slideleft" />
         </figure>
        </div>
     </main>
