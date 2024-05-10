68f9c09a3bc53fc4af79b513ea52d2bb65f046fd
AyushSths <123633939+AyushSths@users.noreply.github.com>
2024-05-10 11:28:00 +0545

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,12 +6,12 @@ import Vintage from '../Components/Vintage'
 import Customization from '../Components/Customization'
 import Automotive from '../Components/Automotive'
 import MoreMotorsports from '../Components/MoreMotorsports'
-import Most_read_section from '../Components/Most_read_section'
-import Categories_section from '../Components/Categories_section'
-import Comparisons_section from '../Components/Comparisons_section'
-import Automative_events from '../Components/Automative_events'
-import Recent_news from '../Components/Recent_news'
-import Car_details from '../Components/Car_details'
+import Most_read_section from '../components/Most_read_section'
+import Categories_section from '../components/Categories_section'
+import Comparisons_section from '../components/Comparisons_section'
+import Automative_events from '../components/Automative_events'
+import Recent_news from '../components/Recent_news'
+import Car_details from '../components/Car_details'
 
 const Home = () => {
   return (
